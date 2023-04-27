f1d45c5b121bbe533b002dfb4149e632c640db95
Elina <madelane.el@gmail.com>
2023-04-27 16:08:29 +0300

    Implement index and entry pages

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title:
+        content = util.get_entry(title)
+        if content is None:
+            return render(request,"encyclopedia/error.html",{
+                "message":"The requested page was not found."
+            })
+        else:
+            return render(request, "encyclopedia/entry.html",{
+                "title":title,
+                "content":content
+            })
+    else:
+        return redirect('index')
+
