72eb0c7b78d560a198b20f8ea5d3021d3f3637cc
Elina <madelane.el@gmail.com>
2023-04-28 12:52:03 +0300

    Add search functionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -3,6 +3,8 @@ import markdown
 
 from . import util
 
+
+# Convert markdown to HTML
 def convert_markdown(title):
     content = util.get_entry(title)
     markdowner =  markdown.Markdown()
@@ -12,12 +14,13 @@ def convert_markdown(title):
         return markdowner.convert(content)
     
 
-
+# List entry items in the index.html
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+# Render entry page if it exists
 def entry(request, title):
     if title:
         html_content = convert_markdown(title)
@@ -32,6 +35,44 @@ def entry(request, title):
             })
     else:
         return redirect('index')
+
+# Search functionality
+def search(request):
+    # if something was submitted in the search bar
+    if request.method == "POST":
+        query = request.POST['q']
+        all_entries = util.list_entries()
+        matching_entries = []
+
+        #if any entries fully or partially match the query, add them to matching_entries list
+        for entry in all_entries:
+            if query.lower() in entry.lower():
+                matching_entries.append(entry)
+
+        # if there are matching entries
+        if matching_entries:
+            #if there is an exact match, redirect to the entry page 
+            if query in matching_entries:
+                return render(request, "encyclopedia/entry.html",{
+                    "title":query,
+                    "content":convert_markdown(query)
+                })
+            # if there are only partial matches, show a page with all results
+            else:
+                return render(request,"encyclopedia/search.html",{
+                    "entries":matching_entries,
+                    "query":query
+                })
+        # if there are no matches at all, show an error message
+        else:
+            return render(request,"encyclopedia/error.html",{
+                "message":"No entries were found"
+                
+            })
+        
+
+            
+
 
 
 
