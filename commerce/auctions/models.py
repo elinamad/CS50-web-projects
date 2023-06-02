ce2d8eb2a19bb158ba6fbf237d6dd057972cce53
Elina <madelane.el@gmail.com>
2023-06-02 15:33:59 +0300

    add more information display on index page

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
 class Listings(models.Model):
     listing_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE, related_name="added_listings")
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
     isactive = models.BooleanField(default=False)
     current_price = models.DecimalField(max_digits=6,decimal_places=2)
     description = models.TextField()
@@ -20,6 +20,7 @@ class Listings(models.Model):
     ]
     category = models.CharField(max_length=20,choices=category_choices,default="fashion", blank=True)
     img = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return f"{self.title}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -14,16 +15,11 @@ from .forms import ListingForm
 
 def index(request):
 
-    listings = Listings.objects.all().filter(isactive = True)
-    description = [listing.description for listing in listings]
-    price = [listing.current_price for listing in listings]
-    img = [listing.img for listing in listings]
+    listings = Listings.objects.all().filter(isactive = True).order_by('-created')
     if listings.exists():
         return render(request,"auctions/index.html",{
             "listings": listings,
-            "description": description,
-            "current_price": price,
-            "img": img
+
         })
     else:
         return render(request,"auctions/index.html",{
@@ -81,21 +77,41 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
-    
+
+@login_required(login_url="/login")
 def newlisting(request):
+    form = ListingForm(request.POST or None)
     if request.method == "GET":
-        form = ListingForm(request.POST)
         return render(request,"auctions/newlisting.html",{
             'form':form
         })
     else:
-        title = request.POST['title']
-        description = request.POST['description']
-        price = request.POST['price']
-        if request.POST['category']:
-            category = request.POST['category']
-        if request.POST['img']:
-            img = request.POST['img']
+        submitbutton = request.POST.get("submit")
+        title = ''
+        description = ''
+        category = ''
+        price = ''
+        if form.is_valid():
+            title = form.cleaned_data['listing_title']
+            description = form.cleaned_data['listing_description']
+            category = form.cleaned_data['listing_category']
+            price = form.cleaned_data['listing_price']
+        newlisting = Listings(
+            user_id = request.user,
+            title = title,
+            description = description,
+            category = category,
+            current_price = price,
+            isactive = True
+        )
+        newlisting.save()
+        return HttpResponseRedirect(reverse("index"))
+
+
+            
+
+
+
         
 
     
