c8536bf0f9034466303137efbd5e7d18fa991487
Elina <madelane.el@gmail.com>
2023-06-08 14:24:23 +0300

    Add send email functionality

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  //send email button
+  document.querySelector('#compose-form').addEventListener('submit',()=> send_email(event));
 });
 
 function compose_email() {
@@ -30,4 +33,30 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(){
+
+  //get the input values
+  let recipientsValue = document.querySelector('#compose-recipients').value;
+  let subjectValue = document.querySelector('#compose-subject').value;
+  let bodyValue = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method:'POST',
+    body: JSON.stringify({
+      recipients:recipientsValue,
+      subject:subjectValue,
+      body:bodyValue
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+
+  //Prevent refresh and show sent emails
+  event.preventDefault();
+  load_mailbox('sent');
+
 }
